0599184b741cd267b2b7cd230e6db4efc8c5c91b
Lessmess125 <estefania.lesmesx3@gmail.com>
2021-07-14 19:01:12 -0500

    email_user_saved

---
+++ src/secondClass.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class secondClass {
+    public static void main(String args[]) {
+
+        //Scanner scanner = new Scanner(System.in);
+        //Scanner is an object from java.util library, scanner is the variable,
+        //new creates new objects; system.in is a constrcutor. CHeck this. 
+        //String Enter = scanner.nextLine(); //Nextline is a method. Also exist nextInt..
+        //System.out.println(Enter);
+
+        //CONDITIONALS IN JAVA 
+        if (5 > 3){
+            System.out.println("Hello");    
+        } else if (4 > 1){
+            System.out.println("Chaolete");
+        } else{
+            System.out.println("h");
+      }
+    
+  
+
+    //FOR LOOPS in java
+
+    for(int i=0;i<7;i++){
+        System.out.println(i);
+    }
+
+  
+
+
+//DO WHILE IN JAVA
+
+int number = 5;
+
+do{
+    System.out.println(":)");
+}while(number>8);
+
+
+}
+
+}
+     
+
+       
---
+++ src/sw.java
@@ -0,0 +1,20 @@
+public class sw {
+    public static void main(String args[]) {
+        String animal = "Dog";
+//SWITCH is also a way of conditional
+switch(animal) {
+  case "Dog":
+        System.out.println("Woff");
+        break;
+   case "Cat":
+       System.out.println("Gatoniel");
+        break;
+    default:
+       System.out.println("Nothing");
+}
+
+ }
+}
+
+
+
